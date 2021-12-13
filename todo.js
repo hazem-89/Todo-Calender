6494542def1488d02f512a2a8350e9896f3d9d73
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-14 00:00:45 +0100

    update

--- todo.js
+++ todo.js
@@ -2,7 +2,9 @@ window.addEventListener('load', mainToDo);
 
 function mainToDo() {
     addEventListeners();
-    todoSummary();
+    addTodoToList()
+    addTodo()
+    deleteTodoFromList()
 }
 
 function addEventListeners() {
@@ -10,7 +12,9 @@ function addEventListeners() {
     form.addEventListener('submit', inputFieldToDoList);
     const toggleButton = document.getElementById('toggle-button');
     toggleButton.addEventListener('click', toggleForm)
-
+    let test = document.querySelector('form');
+    test.addEventListener('submit', addTodo);
+    
 }
 
 function inputFieldToDoList(e) {
@@ -22,16 +26,9 @@ function inputFieldToDoList(e) {
 function toggleForm() {
     const form = document.getElementsByClassName('todo-box')[0];
     form.classList.toggle('hide')
+    
+    
 }
-
-function myFunction() {
-    var x = document.getElementById("toggle-button");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
 const todos = [
     {
@@ -47,9 +44,67 @@ const todos = [
         date: '2021-12-10',
     }
 ];
+//funktionen som skapar list-elementet. 
+function addTodoToList() {
+    // Hämta UL från html, 
+    const ulTodo = document.getElementById('todoULDOM');
+    ulTodo.innerHTML = "";
 
-const todosByDay = todos.filter((todo) => todo.date === "2021-12-10")
+    // loopa igenom arrayen med "todo" objekten
+    for (const todo of todos) {
+        
+        //skapar ett list-element ("Li") för varje  objekt "todo" ur arrayen todos och skriver ut "title" ur objektet i DOMen. samt
+        // lägger till en knapp som har funktion on click. 
+        const liTodo = document.createElement("li");
+        liTodo.innerHTML = todo.title + `<button onclick=deleteTodoFromList() class="deleteTodo">X</button>`;
+        liTodo.className = "list-item"
+       
+        
+        // lägg till li-elementet i UL'en
+        ulTodo.append(liTodo);
+    }  
+}
 
+
+//funktionen som gör så att ett nytt list-element skapas. 
+function addTodo(){
+    let todo = document.getElementById('text');
+    console.log(todo.value);
+    todos.push ({title:todo.value, date: new Date()});//nu så läggs "date" objektet till med datumet det är i nutid när man trycker på knappen.
+    addTodoToList()                                  // det kvarstår nu att lösa så att den lägg tills med rätt datum. 
+    console.log(todos)
+
+}
+
+// en funktion som tar bort "todo" från listan samt tar bort onjektet från arrayen. från arrayen. 
+function deleteTodoFromList(){
+    let buttons = document.getElementsByClassName("deleteTodo")
+    console.log(buttons)
+
+ let removeTodo = document.getElementsByClassName("deleteTodo");
+
+for (let i = 0; i < removeTodo.length; i++) {
+  removeTodo[i].onclick = function(e) {
+    var div = this.parentElement;
+    div.style.display = "none";
+    var index = e.target.getAttribute('value');
+    todos.splice(index, 1);
+    console.log(todos)
+  }
+}
+ 
+}
+
+
+
+function myFunction() {
+    var x = document.getElementById("toggle-button");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
 function todoSummary() {
 
